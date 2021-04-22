e51f283a1fb599cd7ed5ea435fb04c118526d42b
RaySherb <ras0032@alumni.uah.edu>
2021-04-21 21:53:45 -0500

    reformatting to use tidy framework

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
 # Load libraries
 require(quantmod)
 require(PerformanceAnalytics)
-
+require(tidyverse)
 # Step 1: get the data
 ###############################################################################
 # Start date
@@ -16,44 +16,81 @@ colSums(is.na(SPY))
 chartSeries(SPY$Close, type="line")
 addTA(SPY$Adjusted, on=1)
 addTA(SPY$Volume)
-# Adjust for splits & dividends
-SPY$Mult <- SPY$Adjusted/SPY$Close
-SPY$Price <- SPY$Close
-SPY$OpenPrice <- SPY$Open
-SPY$Open <- SPY$Open*SPY$Mult
-SPY$High <- SPY$High*SPY$Mult
-SPY$Low <- SPY$Low*SPY$Mult
-SPY$Close <- SPY$Adjusted
-SPY$Adjusted <- NULL
+
+SPY <- as_tibble(SPY, rownames=NA) %>% rownames_to_column(var='date') %>%
+  mutate(date=as.Date(date),
+         Mult=Adjusted/Close,
+         Price=Close,
+         OpenPrice=Open,
+         Open=Open*Mult,
+         High=High*Mult,
+         Low=Low*Mult,
+         Close=Adjusted) %>% select(-Adjusted)
 
 # Step 2: Create indicators (TTR package included in quantmod)
 ###############################################################################
+# Note: TTR doesn't require xts/zoo
 SPY.MACD <- MACD(SPY$Close)
+# Replace NA's with 0
 SPY.MACD[is.na(SPY.MACD)] <- 0
+# Add as columns to SPY tibble
+SPY <- SPY %>% cbind(SPY.MACD)
+# Remove unnecessary object
+rm(SPY.MACD)
+
 # Plot indicator w/ subplot of price
-chart_Series(SPY.MACD$macd)
-add_TA(SPY.MACD$signal, col='blue', on=1)
-add_TA(SPY$Close)
+# Xts objects required
+graph <- xts(SPY[,-1], order.by = SPY[,1])
+chart_Series(graph$macd)
+add_TA(graph$signal, col = 'blue', on=1)
+add_TA(graph$Close)
 
 # Step 3: Construct your trading rule
 ###############################################################################
-# If macd is above signal then sig = 1, else sig = 0
-sig <- Lag(ifelse(SPY.MACD$macd > SPY.MACD$signal, 1, 0))
-# If sig = 1 then buy and hold, else sell
+# If, at close, the macd is above signal then sig = 1, else sig = 0
+SPY$sig <- ifelse(SPY$macd > SPY$signal, 1, 0)
+# Bullish Crossover
+SPY.MACD$bull <- ifelse(SPY.MACD$sig == 1 & stats::lag(SPY.MACD$sig) == 0, 1, 0) #stats::lag is required due to dplyr::lag causing error
+# Bearish Crossover
+SPY.MACD$bear <- ifelse(SPY.MACD$sig == 0 & stats::lag(SPY.MACD$sig) == 1, -1, 0)
+# Combine Crossovers
+SPY.MACD$cross <- SPY.MACD$bull + SPY.MACD$bear
+# Erase extra columns
+SPY.MACD$bull <- NULL
+SPY.MACD$bear <- NULL
+
+
+# Attempt 3
+#---------------------------------------------------------
+# Zoo/xts is hard to work with. Temporary Change to tibble/data.frame for manipulation
 
 
 
 
+# Attempt 2
+#---------------------------------------------------------
+# https://stackoverflow.com/questions/28886253/dplyr-mutate-in-zoo-object
+SPY.MACD$cash <- 5000
+SPY.MACD$position <- 0
 
-# Step 4: The trading rules/equity curve
-###############################################################################
-ret <- ROC(Cl(SPY))*sig
-ret <- ret['2017-06-02/2021-04-20']
-eq <- exp(cumsum(ret))
-plot(eq)
+sell.cash <- function(open, position, cash){cash = cash + (position * open)}
+buy.position <- function(open, cash){position = round(cash/open)}
+buy.cash <- function(open, position, cash){cash = cash - (position * open)}
 
-# Step 5: Evaluate strategy performance
-###############################################################################
-table.Drawdowns(ret, top=10)
-table.DownsideRisk(ret)
-charts.PerformanceSummary(ret)
+SPY.MACD <- SPY.MACD %>% transform(trigger = lag(cross))
+
+SPY.MACD <- SPY.MACD %>% transform(position = ifelse(trigger==1,
+                                                     buy.position(SPY$Open, stats::lag(cash)),
+                                                     ifelse(trigger==0,
+                                                            stats::lag(position),
+                                                            0)))
+
+
+SPY.MACD <- SPY.MACD %>% transform(cash = ifelse(trigger==1,
+                                                 buy.cash(SPY$Open, position, cash),
+                                                 ifelse(trigger==-1,
+                                                        sell.cash(SPY$Open, stats::lag(position), cash),
+                                                        stats::lag(cash))))
+
+
+SPY.MACD
